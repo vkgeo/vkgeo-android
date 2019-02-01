d2bea6ed24071a77495f366231c83b44bf861b15
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-02-01 14:10:37 +0300

    Separate .ts for src and qml parts.

---
+++ translations/vkgeo_ru.qml.ts
@@ -0,0 +1,196 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>FriendsSwipe</name>
+    <message>
+        <source>Invitation sent</source>
+        <translation>Приглашение отправлено</translation>
+    </message>
+    <message>
+        <source>Quick search</source>
+        <translation>Быстрый поиск</translation>
+    </message>
+    <message>
+        <source>Invite friend</source>
+        <translation>Пригласить друга</translation>
+    </message>
+    <message>
+        <source>Do you want to send an invitation to this friend?</source>
+        <translation>Вы действительно хотите отослать приглашение этому другу?</translation>
+    </message>
+    <message>
+        <source>I invite you to install the VKGeo Friends on Map app and join the community: https://vkgeo.sourceforge.io/</source>
+        <translation>Я приглашаю Вас установить приложение VKGeo Друзья на Карте и присоединиться к сообществу: https://vk.com/vkgeo_app</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Sign in with your VK account</source>
+        <translation>Войдите в свой профиль ВКонтакте</translation>
+    </message>
+    <message>
+        <source>Privacy policy</source>
+        <translation>Политика конфиденциальности</translation>
+    </message>
+    <message>
+        <source>https://vkgeo.sourceforge.io/en/privacy.php</source>
+        <translation>https://vkgeo.sourceforge.io/ru/privacy.php</translation>
+    </message>
+</context>
+<context>
+    <name>MapSwipe</name>
+    <message>
+        <source>Update location data</source>
+        <translation>Обновить данные о местоположении</translation>
+    </message>
+    <message>
+        <source>Do you want to update location data in your VK profile now?</source>
+        <translation>Вы действительно хотите сейчас обновить данные о местоположении в вашем профиле ВКонтакте?</translation>
+    </message>
+</context>
+<context>
+    <name>ProfilePage</name>
+    <message>
+        <source>Profile info</source>
+        <translation>Информация профиля</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <source>Locate on map</source>
+        <translation>Показать на карте</translation>
+    </message>
+    <message>
+        <source>Open profile</source>
+        <translation>Открыть профиль</translation>
+    </message>
+    <message>
+        <source>Get directions</source>
+        <translation>Построить маршрут</translation>
+    </message>
+    <message>
+        <source>Last update at: %1</source>
+        <translation>Последнее обновление: %1</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsSwipe</name>
+    <message>
+        <source>Group membership request sent</source>
+        <translation>Запрос на вступление в группу отправлен</translation>
+    </message>
+    <message>
+        <source>You can see the location of trusted friends, if they also treat you as a trusted friend, and they &lt;b&gt;can see your location&lt;/b&gt;.</source>
+        <translation>Вы можете видеть местоположение доверенных друзей, если они также считают вас доверенным другом, и они &lt;b&gt;могут видеть ваше местоположение&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Currently you can have up to %1 trusted friends.</source>
+        <translation>В данный момент вы можете иметь не более %1 доверенных друзей.</translation>
+    </message>
+    <message>
+        <source>Currently you can&apos;t have trusted friends.</source>
+        <translation>В данный момент вы не можете иметь доверенных друзей.</translation>
+    </message>
+    <message>
+        <source>Trusted friends list</source>
+        <translation>Список доверенных друзей</translation>
+    </message>
+    <message>
+        <source>You can see the location of tracked friends, if they treat you as a trusted friend, but they &lt;b&gt;cannot see your location&lt;/b&gt;.</source>
+        <translation>Вы можете видеть местоположение отслеживаемых друзей, если они считают вас доверенным другом, но они &lt;b&gt;не могут видеть ваше местоположение&lt;/b&gt;.</translation>
+    </message>
+    <message>
+        <source>Currently you can have up to %1 tracked friends.</source>
+        <translation>В данный момент вы можете иметь не более %1 отслеживаемых друзей.</translation>
+    </message>
+    <message>
+        <source>Tracked friends list</source>
+        <translation>Список отслеживаемых друзей</translation>
+    </message>
+    <message>
+        <source>Support</source>
+        <translation>Поддержка</translation>
+    </message>
+    <message>
+        <source>https://vkgeo.sourceforge.io/</source>
+        <translation>vkgeo_app</translation>
+    </message>
+    <message>
+        <source>View my VK profile</source>
+        <translation>Посмотреть мой профиль ВКонтакте</translation>
+    </message>
+    <message>
+        <source>Log out of VK</source>
+        <translation>Выход из ВКонтакте</translation>
+    </message>
+    <message>
+        <source>Join group</source>
+        <translation>Вступить в группу</translation>
+    </message>
+    <message>
+        <source>Join VKGeo Friends on Map group</source>
+        <translation>Вступить в группу VKGeo Друзья на Карте</translation>
+    </message>
+    <message>
+        <source>Do you want to join the VKGeo Friends on Map group?</source>
+        <translation>Вы действительно хотите вступить в группу VKGeo Друзья на Карте?</translation>
+    </message>
+    <message>
+        <source>Additional settings</source>
+        <translation>Дополнительные настройки</translation>
+    </message>
+    <message>
+        <source>Currently you can&apos;t have tracked friends.</source>
+        <translation>В данный момент вы не можете иметь отслеживаемых друзей.</translation>
+    </message>
+</context>
+<context>
+    <name>TrackedFriendsPage</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <source>Tracked friends</source>
+        <translation>Отслеживаемые друзья</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <source>Quick search</source>
+        <translation>Быстрый поиск</translation>
+    </message>
+</context>
+<context>
+    <name>TrustedFriendsPage</name>
+    <message>
+        <source>Cancel</source>
+        <translation>Отменить</translation>
+    </message>
+    <message>
+        <source>Trusted friends</source>
+        <translation>Доверенные друзья</translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <source>Quick search</source>
+        <translation>Быстрый поиск</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Friends on Map</source>
+        <translation>Друзья на Карте</translation>
+    </message>
+</context>
+</TS>
