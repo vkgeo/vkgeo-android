eaa67d87e29443644c0bef9f945fc56a7a69dbe9
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-03-10 02:44:26 +0300

    Replace plain strings by constants.

--- android/source/src/com/derevenetz/oleg/vkgeo/gplay/VKGeoReceiver.java
+++ android/source/src/com/derevenetz/oleg/vkgeo/gplay/VKGeoReceiver.java
@@ -10,8 +10,8 @@ public class VKGeoReceiver extends BroadcastReceiver
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED") ||
-            intent.getAction().equals("android.intent.action.MY_PACKAGE_REPLACED")) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
+            intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(new Intent(context, VKGeoService.class));
             } else {
