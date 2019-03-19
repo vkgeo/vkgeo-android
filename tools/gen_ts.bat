fadaa165e23878b9fe27ec49c40d7150fd65a089
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-03-19 17:06:00 +0300

    Update scripts for Qt 5.12.2.

--- tools/gen_ts.bat
+++ tools/gen_ts.bat
@@ -1,4 +1,4 @@
-PATH=%PATH%;C:\Qt\5.12.1\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
+PATH=%PATH%;C:\Qt\5.12.2\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
 
 lupdate ..\vkgeo.pro -ts ..\translations\vkgeo_ru.src.ts
 lupdate ..\qml       -ts ..\translations\vkgeo_ru.qml.ts
