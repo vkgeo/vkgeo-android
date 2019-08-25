8fc5a20e6c044f8c2510fc5530cf5510eda0e4e7
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-08-25 22:14:31 +0300

    Always add locations to .ts files.

--- tools/gen_ts.bat
+++ tools/gen_ts.bat
@@ -1,5 +1,5 @@
 PATH=%PATH%;C:\Qt\5.12.4\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
 
-lupdate ..\vkgeo.pro -ts ..\translations\vkgeo_ru.src.ts
+lupdate -locations absolute ..\vkgeo.pro -ts ..\translations\vkgeo_ru.src.ts
 
 lconvert ..\translations\vkgeo_ru.src.ts ..\translations\vkgeo_ru.qt.ts -o ..\translations\vkgeo_ru.ts
