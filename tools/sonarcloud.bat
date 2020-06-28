f0ae31ee34158d76b84e8077269705e11051a42d
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-06-28 14:42:36 +0300

    Update SonarCloud script for Qt 5.12.9 and new directory layout.

--- tools/sonarcloud.bat
+++ tools/sonarcloud.bat
@@ -7,7 +7,7 @@ SET ANDROID_NDK_HOST=windows-x86_64
 SET SONARCLOUD_ROOT=D:\Users\%USERNAME%\Documents\SonarCloud
 SET ANDROID_PLATFORM=android-29
 
-SET PATH=%PATH%;C:\Qt\5.12.8\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows-x86_64\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-4.3.0.2102-windows\bin
+SET PATH=%PATH%;C:\Qt\5.12.9\android_armv7\bin;%ANDROID_NDK_ROOT%\prebuilt\windows-x86_64\bin;%SONARCLOUD_ROOT%\build-wrapper-win-x86;%SONARCLOUD_ROOT%\sonar-scanner-4.3.0.2102-windows\bin
 
 IF NOT "%~1"=="" (
         CD .. ^
@@ -26,7 +26,7 @@ IF NOT "%~1"=="" (
                           -D"sonar.organization=vkgeo-github" ^
                           -D"sonar.sources=." ^
                           -D"sonar.sourceEncoding=UTF-8" ^
-                          -D"sonar.exclusions=android/source/vksdk_library/**/*.java,qml/**/*,translations/*" ^
+                          -D"sonar.exclusions=3rdparty/**/*,android/source/3rdparty/**/*,qml/**/*,translations/*" ^
                           -D"sonar.cfamily.build-wrapper-output=.sonarbuild\\bw-output" ^
                           -D"sonar.cfamily.cache.enabled=false" ^
                           -D"sonar.cfamily.threads=%NUMBER_OF_PROCESSORS%" ^
