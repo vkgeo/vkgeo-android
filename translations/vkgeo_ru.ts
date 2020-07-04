824cabbe0638a7246dac29ee6cba3925df59d177
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2020-07-05 01:47:01 +0300

    "Press and hold" -> "Press and hold down".

--- translations/vkgeo_ru.ts
+++ translations/vkgeo_ru.ts
@@ -273,7 +273,7 @@
     </message>
     <message>
         <location filename="../qml/Core/Main/SettingsSwipe.qml" line="266"/>
-        <source>&lt;b&gt;Pass this public key&lt;/b&gt; to trusted and tracked friends who use encryption and &lt;b&gt;whose location you want to see&lt;/b&gt;. Press and hold your finger on the partial public key text below to copy the public key to the clipboard.</source>
+        <source>&lt;b&gt;Pass this public key&lt;/b&gt; to trusted and tracked friends who use encryption and &lt;b&gt;whose location you want to see&lt;/b&gt;. Press and hold down your finger on the partial public key text below to copy the public key to the clipboard.</source>
         <translation>&lt;b&gt;Передайте этот открытый ключ&lt;/b&gt; доверенным и отслеживаемым друзьям, которые используют шифрование, и &lt;b&gt;местоположение которых вы хотите видеть&lt;/b&gt;. Вы можете скопировать открытый ключ в буфер обмена длительным нажатием пальца на приведенном ниже частичном тексте открытого ключа.</translation>
     </message>
     <message>
