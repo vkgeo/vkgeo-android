086d924ddad934562f1a98021c476d4eb38a3cac
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-02-21 15:56:40 +0300

    Don't start VKGeoService in VKGeoReceiver: it should be already done in VKGeoApplication.onCreate().

--- android/source/src/com/derevenetz/oleg/vkgeo/stdalone/VKGeoReceiver.java
+++ android/source/src/com/derevenetz/oleg/vkgeo/stdalone/VKGeoReceiver.java
@@ -3,20 +3,13 @@ package com.derevenetz.oleg.vkgeo.stdalone;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 public class VKGeoReceiver extends BroadcastReceiver
 {
     @Override
+    @SuppressWarnings("UnsafeProtectedBroadcastReceiver")
     public void onReceive(Context context, Intent intent)
     {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
-            intent.getAction().equals(Intent.ACTION_MY_PACKAGE_REPLACED)) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.startForegroundService(new Intent(context, VKGeoService.class));
-            } else {
-                context.startService(new Intent(context, VKGeoService.class));
-            }
-        }
+        // Ignore
     }
 }
