696472285c1fe5ef25366b1f561f63be8f4c0c84
Oleg Derevenetz <oleg-derevenetz@yandex.ru>
2021-06-14 01:31:43 +0300

    Update scripts for Qt 5.12.11.

--- tools/gen_ts.bat
+++ tools/gen_ts.bat
@@ -1,4 +1,4 @@
-SET PATH=%PATH%;C:\Qt\5.12.10\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
+SET PATH=%PATH%;C:\Qt\5.12.11\mingw73_64\bin;C:\Qt\Tools\mingw730_64\bin
 
 lupdate -locations absolute ..\vkgeo.pro -ts ..\translations\vkgeo_ru.src.ts
 
